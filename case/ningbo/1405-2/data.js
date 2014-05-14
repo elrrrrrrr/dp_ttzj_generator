68ee32ef746f119cacbad3551af8707553cca44a
elrrrrrrr <elrrrrrrr@gmail.com>
2014-05-14 10:55:54 +0800

    add case

---
+++ case/ningbo/1405-2/data.js
@@ -0,0 +1,71 @@
+pageTracker._trackPageview("events/market/ttzj/ningbo");
+
+window.GROUP_ID = 195;
+
+window.storeList = [{
+    "name": "醉角落",
+    "link": "http://www.dianping.com/shop/17705399",
+    "img": "img/1/logo.jpg",
+    "introduction": "在醉角落自视觉、听觉、触觉、嗅觉、味觉引发的五感享受，从而感受到醉角落 尽心营造的有感觉、更贴心的用餐氛围。无论独乐或同欢，每个人都可以在这里体每个人都可以在这...",
+    "more": "在醉角落自视觉、听觉、触觉、嗅觉、味觉引发的五感享受，从而感受到醉角落 尽心营造的有感觉、更贴心的用餐氛围。无论独乐或同欢，每个人都可以在这里体每个人都可以在这里体验一次时尚、知性的感官之旅。",
+    "promotion": "",
+    "coupon": "",
+    "booking": "",
+    "member": "",
+    "carousel": ["img/1/1.jpg", "img/1/2.jpg", "img/1/3.jpg", "img/1/4.jpg", "img/1/5.jpg"]
+}, {
+    "name": "锦绣人家主题餐厅",
+    "link": "http://www.dianping.com/shop/3369097",
+    "img": "img/2/logo.jpg",
+    "introduction": "坐落在桑田路845号路边的锦绣人家，门面宽敞，装潢气派，米黄色的灯光，大气中透着一点典雅，夜色中显得尤其抢眼。装修上大面积运用了镜面玻璃和不锈钢，视觉延展性很强...",
+    "more": "坐落在桑田路845号路边的锦绣人家，门面宽敞，装潢气派，米黄色的灯光，大气中透着一点典雅，夜色中显得尤其抢眼。装修上大面积运用了镜面玻璃和不锈钢，视觉延展性很强，外加米黄色灯光一打，外面看餐厅是富丽堂皇，里面看餐厅则是温馨而不失精致。",
+    "promotion": "",
+    "coupon": "http://t.dianping.com/deal/5055715",
+    "booking": "",
+    "member": "",
+    "carousel": ["img/2/1.jpg", "img/2/2.jpg", "img/2/3.jpg", "img/2/4.jpg", "img/2/5.jpg", "img/2/6.jpg", "img/2/7.jpg", "img/2/8.jpg"]
+}, {
+    "name": "丽蓓卡SPA",
+    "link": "http://www.dianping.com/shop/8014602",
+    "img": "img/3/logo.jpg",
+    "introduction": "丽蓓卡 REBECCA SPA毗邻宁波各市中心顶级繁华区域,环境优雅高档舒适，集女子养颜，护肤美体，减肥瘦身纤体，女性特别护理养生，全身SPA为一体，的专业女子...",
+    "more": "丽蓓卡 REBECCA SPA毗邻宁波各市中心顶级繁华区域,环境优雅高档舒适，集女子养颜，护肤美体，减肥瘦身纤体，女性特别护理养生，全身SPA为一体，的专业女子养颜生活会所，高品质享受，亲切专业化的服务理念，瑞士、德国原装进口全球十大专业线国际护肤品牌保证、完善服务信誉保障。",
+    "promotion": "",
+    "coupon": "http://t.dianping.com/deal/2268005#",
+    "booking": "",
+    "member": "",
+    "carousel": ["img/3/1.jpg", "img/3/2.jpg", "img/3/3.jpg", "img/3/4.jpg", "img/3/5.jpg"]
+}, {
+    "name": "SOR祛痘中心",
+    "link": "",
+    "img": "img/4/logo.jpg",
+    "introduction": "SOR（索尔）祛痘自创立伊始就一直秉持物尽天然的原则，通过现代生物技术的前沿科技力量同步全球护肤科技，并不断开拓创新，始终坚持务实、严谨、精确的产品研发准则，针...",
+    "more": "SOR（索尔）祛痘自创立伊始就一直秉持物尽天然的原则，通过现代生物技术的前沿科技力量同步全球护肤科技，并不断开拓创新，始终坚持务实、严谨、精确的产品研发准则，针对皮肤问题进行预防、修护和再造。 SOR（索尔）品牌致力于痘肌护理，凭借良好的市场口碑和手法备受广大“痘友们”的追捧拥戴，掀起了痘肌护理风潮、翻开了重拾美丽容颜的新篇章，帮助无数痘友重新树立对于美的自信。",
+    "promotion": "",
+    "coupon": "http://t.dianping.com/deal/5059703",
+    "booking": "",
+    "member": "",
+    "carousel": ["img/4/1.jpg", "img/4/2.jpg", "img/4/3.jpg", "img/4/4.jpg"]
+}, {
+    "name": "空客主题餐厅",
+    "link": "http://www.dianping.com/shop/17008500",
+    "img": "img/5/logo.jpg",
+    "introduction": "空客主题餐厅坐落于鄞州区宁南北路1288号联盛广场C2区1楼8-10号，登上空客，享受头等舱的用餐环境，下了飞机漫步于巴厘岛的沙滩吹吹海风，转个身便是时尚巴黎，...",
+    "more": "空客主题餐厅坐落于鄞州区宁南北路1288号联盛广场C2区1楼8-10号，登上空客，享受头等舱的用餐环境，下了飞机漫步于巴厘岛的沙滩吹吹海风，转个身便是时尚巴黎，让你享受一次精致的美食之旅",
+    "promotion": "http://www.dianping.com/promo/221077?source=1",
+    "coupon": "http://t.dianping.com/deal/2274061",
+    "booking": "",
+    "member": "",
+    "carousel": ["img/5/1.jpg", "img/5/2.jpg", "img/5/3.jpg", "img/5/4.jpg", "img/5/5.jpg"]
+}, {
+    "name": "TREE&HOME",
+    "link": "",
+    "img": "img/6/logo.jpg",
+    "introduction": "TREE&HOME來了。TREE&HOME本來就是個雜貨店，從頭到尾都是，雜貨店本來什麽都賣，只要是喜歡的好看的。anyway，這些都不是重點了。于我而言，我覺...",
+    "more": "TREE&HOME來了。TREE&HOME本來就是個雜貨店，從頭到尾都是，雜貨店本來什麽都賣，只要是喜歡的好看的。anyway，這些都不是重點了。于我而言，我覺得TREE&HOME比MUJI鮮活，比IKEA靜謐，最重要的是，TREE&HOME不是從北歐或日本哪哪哪，漂過來的品牌它是“在寧波”的TREE&HOME。生活&雜貨&良品有這些，才是TREE&HOME",
+    "promotion": "",
+    "coupon": "",
+    "booking": "",
+    "member": "",
+    "carousel": ["img/6/1.jpg", "img/6/2.jpg", "img/6/3.jpg", "img/6/4.jpg", "img/6/5.jpg"]
+}];
