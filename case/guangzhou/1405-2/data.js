3b58f84e03bcb232de71bfb4631d45830a02ecc9
elrrrrrrr <elrrrrrrr@gmail.com>
2014-05-23 15:33:13 +0800

    add case

---
+++ case/guangzhou/1405-2/data.js
@@ -0,0 +1,82 @@
+pageTracker._trackPageview("events/market/ttzj/guangzhou");
+
+window.GROUP_ID = 196;
+
+window.storeList = [{
+    "name": "正本日本料理",
+    "link": "http://www.dianping.com/shop/6268176",
+    "img": "img/1.jpg",
+    "introduction": "正本日本料理立志做广州最正宗的日本料理，其食材与调味料90%取材于日本，保证菜品做到最日式的口味，并且每周两次从日本空运新鲜鱼生回店保证你来正本日本料理可享受到...",
+    "more": "正本日本料理立志做广州最正宗的日本料理，其食材与调味料90%取材于日本，保证菜品做到最日式的口味，并且每周两次从日本空运新鲜鱼生回店保证你来正本日本料理可享受到新鲜，当季，美味的生鱼片,店内温馨舒适的榻榻米房更是三五知己小聚与公司商务宴请的理想场所。",
+    "promotion": "",
+    "coupon": "",
+    "booking": "",
+    "member": "",
+    "carousel": []
+}, {
+    "name": "味蜀吾老火锅",
+    "link": "http://www.dianping.com/shop/17815485",
+    "img": "img/2.jpg",
+    "introduction": "味蜀吾老火锅以“诚心做味道，良心做品质”的经营理念，“承千秋巴渝文化、创百年火锅店”为追求。传承火锅文化之精髓，由名师精心调制、口味地道纯正、口感鲜美醇厚，深得...",
+    "more": "味蜀吾老火锅以“诚心做味道，良心做品质”的经营理念，“承千秋 巴渝文化、创百年火锅店”为追求。传承火锅文化之精髓，由名师精 心调制、口味地道纯正、口感鲜美醇厚，深得消费者的喜爱。",
+    "promotion": "",
+    "coupon": "",
+    "booking": "",
+    "member": "",
+    "carousel": []
+}, {
+    "name": "毛家湾饭店（海印店）",
+    "link": "http://www.dianping.com/shop/4515375",
+    "img": "img/3.jpg",
+    "introduction": "毛家湾饭店开在毛泽东同志故居（湖南韶山）对面。许多来韶山的客人，慕名到毛家湾饭店就餐，品尝着毛主席生前爱吃的家常菜，追忆着毛主席的丰功伟绩；同时也是一次难得的享...",
+    "more": " 毛家湾饭店开在毛泽东同志故居（湖南韶山）对面。许多来韶山的客 人，慕名到毛家湾饭店就餐，品尝着毛主席生前爱吃的家常菜，追忆 着毛主席的丰功伟绩；同时也是一次难得的享受和心灵陶冶。“毛家 湾饭店”推出了浓郁的具有湖南乡土风味的“毛家菜”。",
+    "promotion": "http://www.dianping.com/promo/217577?source=1",
+    "coupon": "",
+    "booking": "http://www.dianping.com/shop/4515375#reserve",
+    "member": "",
+    "carousel": []
+}, {
+    "name": "菊田寿司",
+    "link": "http://www.dianping.com/shop/13907759",
+    "img": "img/4.jpg",
+    "introduction": "",
+    "more": "",
+    "promotion": "",
+    "coupon": "",
+    "booking": "",
+    "member": "",
+    "carousel": []
+}, {
+    "name": "湛江海哥蠔庄",
+    "link": "http://www.dianping.com/shop/14879551",
+    "img": "img/5.jpg",
+    "introduction": "湛江海哥蠔庄始于1999年，15年的悠久历史，始终专注于生蚝的独家秘方烤制，带给您视觉的冲击，享受味蕾和精神的同步升级。致力于使用早晨最新鲜的海鲜食材，保持最高...",
+    "more": "湛江海哥蠔庄始于1999年，15年的悠久历史，始终专注于生蚝的独家秘方烤制，带给您视觉的冲击，享受味蕾和精神的同步升级。致力于使用早晨最新鲜的海鲜食材，保持最高的新鲜度、纯天然，是业界口碑俱佳的蚝庄。",
+    "promotion": "",
+    "coupon": "http://t.dianping.com/deal/2254091",
+    "booking": "",
+    "member": "",
+    "carousel": []
+}, {
+    "name": "古德菲力健身会所",
+    "link": "http://www.dianping.com/shop/17707449",
+    "img": "img/6.jpg",
+    "introduction": "古德菲力健身会所诞生于广州。凭借优良的健身运动产品品质及先进品牌经营理念，已发展成大型运动、休闲、娱乐的综合性健身会所。我司成立于2004年，目前拥有会所8家，...",
+    "more": "古德菲力健身会所诞生于广州。凭借优良的健身运动产品品质及先进品牌经营理念，已发展成大型运动、休闲、娱乐的综合性健身会所。我司成立于2004年，目前拥有会所8家，遍布广州、深圳、邯郸，拥有员工超过500名。会所交通便利，室内完备的换气通风空调设施，豪华舒适的更衣、淋浴室，时尚健康的网域空间等配套服务，尽显无微不至的关怀。",
+    "promotion": "",
+    "coupon": "http://t.dianping.com/deal/2220294",
+    "booking": "",
+    "member": "",
+    "carousel": []
+}, {
+    "name": "Luxury莱斯利造型设计",
+    "link": "http://www.dianping.com/shop/17780827",
+    "img": "img/7.jpg",
+    "introduction": "莱斯利造型设计（Luxury Hair Salon），作为一间专为城中精英、 外籍人士提供精湛技术及尊贵服务的造型公司，我们以星级的管理手法对内部进行管理。 L...",
+    "more": "莱斯利造型设计（Luxury Hair Salon），作为一间专为城中精英、 外籍人士提供精湛技术及尊贵服务的造型公司，我们以星级的管理手法对内部进行管理。 Luxury Hair Salon是一个提供高水准与杰出服务的美发造型公司。我们为顾客提供量身定做时尚、个性的美发服务。满足并超越顾客、员工的期望是我们的宗旨。我们追求完美的技术、诚信的服务。我们推崇团队合作、鼓励创新。我们勤奋好学，追求自我超越。",
+    "promotion": "http://www.dianping.com/promo/221213?source=1",
+    "coupon": "http://t.dianping.com/deal/6050017",
+    "booking": "",
+    "member": "",
+    "carousel": []
+}];
---
+++ case/shanghai/1405-2/data.js
@@ -0,0 +1,93 @@
+pageTracker._trackPageview("events/market/ttzj/shanghai");
+
+window.GROUP_ID = 197;
+
+window.storeList = [{
+    "name": "避风塘（松江万达店）",
+    "link": "http://www.dianping.com/shop/18244606",
+    "img": "img/1/logo.jpg",
+    "introduction": "沪上著名港式餐馆，以经营风味地道的港式美食、开创沪上休闲时尚餐饮为亮点，自1998年开设第一家锦江店而风靡流行，时至今日全国已逾80家直营店，绝无加盟！",
+    "more": "",
+    "promotion": "",
+    "coupon": "http://t.dianping.com/deal/5500418",
+    "booking": "",
+    "member": "",
+    "carousel": ["img/1/1.jpg", "img/1/2.jpg", "img/1/3.jpg", "img/1/4.jpg", "img/1/5.jpg"]
+}, {
+    "name": "赤坂亭（赤坂亭松江万达店）",
+    "link": "http://www.dianping.com/shop/18245413",
+    "img": "img/2/logo.jpg",
+    "introduction": "赤坂亭品牌创建于2005年5月，是一家具有较高知名度的全国性日式餐饮连锁品牌，主营炭火烧肉、日式铁板烧、日本料理，就餐模式以自助为主。",
+    "more": "",
+    "promotion": "",
+    "coupon": "",
+    "booking": "",
+    "member": "",
+    "carousel": ["img/2/1.jpg", "img/2/2.jpg", "img/2/3.jpg", "img/2/4.jpg", "img/2/5.jpg"]
+}, {
+    "name": "望湘园（松江万达店）",
+    "link": "http://www.dianping.com/shop/18238559",
+    "img": "img/3/logo.jpg",
+    "introduction": "望湘园是以“为平民服务”的餐饮品牌，现已遍布上海、北京、天津等华东、华北的十多个城市，拥有近60家直营店铺。望湘园将湖湘味、生活味、都市味、人情味……融诸味于一炉，广受食客喜爱。 就这湘味.This is it！",
+    "more": "",
+    "promotion": "",
+    "coupon": "",
+    "booking": "",
+    "member": "",
+    "carousel": ["img/3/1.jpg", "img/3/2.jpg", "img/3/3.jpg", "img/3/4.jpg", "img/3/5.jpg"]
+}, {
+    "name": "西贝莜面村（松江万达店）",
+    "link": "http://www.dianping.com/shop/18181556",
+    "img": "img/4/logo.jpg",
+    "introduction": "西贝餐饮管理有限公司发源于内蒙古临河市，迄今已有二十多年的发展历程。西贝餐饮创始人贾国龙先生，经过多年执着坚持、辛勤开拓，从起初的一个“黄土坡小吃店”发展成为全国50多家店面。",
+    "more": "",
+    "promotion": "",
+    "coupon": "",
+    "booking": "",
+    "member": "",
+    "carousel": ["img/4/1.jpg", "img/4/2.jpg", "img/4/3.jpg", "img/4/4.jpg", "img/4/5.jpg"]
+}, {
+    "name": "小米餐厅",
+    "link": "",
+    "img": "img/5/logo.jpg",
+    "introduction": "小米餐厅主营融合菜，创新出“形如淮扬，味在川，色及苏杭，精其粤，地道蜀风，又似湘”的精致美食。餐厅兼具情调与隽雅，用服务提供时尚、多元、精致的美食体验。",
+    "more": "",
+    "promotion": "",
+    "coupon": "",
+    "booking": "",
+    "member": "",
+    "carousel": ["img/5/1.jpg", "img/5/2.jpg", "img/5/3.jpg", "img/5/4.jpg", "img/5/5.jpg"]
+}, {
+    "name": "粤满逸",
+    "link": "http://www.dianping.com/shop/17668917",
+    "img": "img/6/logo.jpg",
+    "introduction": "粤满逸汤馆&茶餐厅既有传统的文化也结合了欧式现代的装修风格，从正宗的港式美食到潮流的设计元素让粤满逸品牌拥有各年龄层的消费群。粤满逸汤馆&茶餐厅坚持持原汁原味，...",
+    "more": "粤满逸汤馆&茶餐厅既有传统的文化也结合了欧式现代的装修风格，从正宗的港式美食到潮流的设计元素让粤满逸品牌拥有各年龄层的消费群。粤满逸汤馆&茶餐厅坚持持原汁原味，从经验丰富的香港师傅和实力超强的管理团队到美籍精英设计师，立志在上海树立一个全新的香港茶餐厅经营模式。",
+    "promotion": "",
+    "coupon": "",
+    "booking": "",
+    "member": "",
+    "carousel": ["img/6/1.jpg", "img/6/2.jpg", "img/6/3.jpg", "img/6/4.jpg"]
+}, {
+    "name": "掌柜的店纯朴中原菜（松江万达店）",
+    "link": "http://www.dianping.com/shop/18269475",
+    "img": "img/7/logo.jpg",
+    "introduction": "店不在大，有客则名；菜不在多，有味则灵。2006年由两位狂热热爱美食的年轻夫妇创建至今，每日顾客盈门，本着价格亲民，菜量实在。环境力求做到中国人看着洋气、外国人...",
+    "more": "店不在大，有客则名；菜不在多，有味则灵。2006年由两位狂热热爱美食的年轻夫妇创建至今，每日顾客盈门，本着价格亲民，菜量实在。环境力求做到中国人看着洋气、外国人看着China、年轻人看着时尚、老年人看着怀旧的装潢风格。追求对待家人与朋友的服务风格，各分店均交通便利。适合朋友小酌，家庭聚会，情侣约会，商务聚餐。人均消费50-60元左右，力争做到全心全意为所有爱好美食的朋友服务。",
+    "promotion": "",
+    "coupon": "",
+    "booking": "",
+    "member": "",
+    "carousel": ["img/7/1.jpg", "img/7/2.jpg", "img/7/3.jpg", "img/7/4.jpg", "img/7/5.jpg"]
+}, {
+    "name": "新发现(万达广场店)",
+    "link": "http://www.dianping.com/shop/17663737",
+    "img": "img/8/logo.jpg",
+    "introduction": "新发现在主打“杭帮菜、民间碳火瓦罐煨汤、家常菜、江浙菜、川菜等”的菜系基础上增加了更多新的时尚元素及时尚菜系，让您在饮食生活中体验更加健康的选择。 新发现斥资在...",
+    "more": "新发现在主打“杭帮菜、民间碳火瓦罐煨汤、家常菜、江浙菜、川菜等”的菜系基础上增加了更多新的时尚元素及时尚菜系，让您在饮食生活中体验更加健康的选择。 新发现斥资在装修上更以超高调的手笔，以怀旧老上海的风格为主调，装修新颖的表现方式营造出舒适、个性、Fashion、原始、健康的就餐氛围。红砖铁墙旧地板将你与现代都市的快节奏生活隔离开来，将这个无时无刻不在用“新”来取代“旧”的年代隐去，恍若时光逆转，与她，一起邂逅在旧上海时代的某个角落，民间碳火瓦罐煨汤更具特色的体现了一个健康养生的地方，在品味中奢华，在忘我中轻轻的享受环境与美食。 ",
+    "promotion": "",
+    "coupon": "",
+    "booking": "",
+    "member": "",
+    "carousel": ["img/8/1.jpg", "img/8/2.jpg", "img/8/3.jpg", "img/8/4.jpg", "img/8/5.jpg"]
+}];
