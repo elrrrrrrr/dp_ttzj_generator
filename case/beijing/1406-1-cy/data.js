6dcb18687a2ce8010ff4b8d388dabef23640ecb4
elrrrrrrr <elrrrrrrr@gmail.com>
2014-06-12 22:51:07 +0800

    add case

---
+++ case/beijing/1406-1-cy/data.js
@@ -0,0 +1,116 @@
+pageTracker._trackPageview("events/market/ttzj/beijing");
+
+window.GROUP_ID = 204;
+
+window.storeList = [{
+    "name": "丽江龙记斑鱼庄（远洋山水店）",
+    "link": "http://www.dianping.com/shop/6090101",
+    "img": "img/1/logo.jpg",
+    "introduction": "龙记斑鱼庄是专门吃鱼的地方，鱼只有一种，云南人叫乌鱼或黑鱼，广西人叫斑鱼，湖南人叫财鱼。其主要来自湖南洞庭湖，素以小鱼、小虾为食，故名又称水老虎。斑鱼一般都在...",
+    "more": "龙记斑鱼庄是专门吃鱼的地方，鱼只有一种，云南人叫乌鱼或黑鱼，广西人叫斑鱼，湖南人叫财鱼。其主要来自湖南洞庭湖，素以小鱼、小虾为食，故名又称水老虎。斑鱼一般都在3斤以上，而且是一鱼三吃，鱼肉切片烫火锅，鱼肉片薄如纸，肉质细嫩，不腥不腻，长煮不烂，肉美汤鲜，还有相当别致的鱼肉刺身。小料是海鲜料与小米辣姜末的双拼，自己可以视个人口味混搭。",
+    "promotion": "http://www.dianping.com/promo/217081?source=6",
+    "coupon": "http://t.dianping.com/deal/235998",
+    "booking": "http://www.dianping.com/shop/6090101#reserve",
+    "member": "http://www.dianping.com/card/detail/c3_b6090101_a22#6090101",
+    "carousel": ["img/1/1.jpg", "img/1/2.jpg", "img/1/3.jpg", "img/1/4.jpg", "img/1/5.jpg", "img/1/6.jpg", "img/1/7.jpg", "img/1/8.jpg"]
+}, {
+    "name": "俏巴妹",
+    "link": "http://www.dianping.com/shop/8003614",
+    "img": "img/3/logo.jpg",
+    "introduction": "俏巴妺是一家以经营烤鱼、香锅、沸腾鱼、烤翅为主的激情菜餐厅。餐厅装修时尚，格调高雅。巴妺的经营理念很简单，就是把普通的菜做得不普通，把人们常规想象的四川风格装修...",
+    "more": "俏巴妺是一家以经营烤鱼、香锅、沸腾鱼、烤翅为主的激情菜餐厅。餐厅装修时尚，格调高雅。巴妺的经营理念很简单，就是把普通的菜做得不普通，把人们常规想象的四川风格装修华丽转身，由现代时尚元素所取代。巴妹的目标消费人群就是城市的小资们。在这些方面，巴妺应该说在京城是开了个先河，近两年虽效仿巴妺的很多，但巴妺也一直在向前跑。 目前，俏巴妹已拥有奥体、国展、安贞、平乐园和soho尚都店五家店面。俏巴妹，随时欢迎您的到来。",
+    "promotion": "http://www.dianping.com/promo/219638?source=1",
+    "coupon": "http://t.dianping.com/deal/5113603",
+    "booking": "",
+    "member": "http://www.dianping.com/card/detail/c3_b8003614_a20#8003614",
+    "carousel": ["img/3/1.jpg", "img/3/2.jpg", "img/3/3.jpg", "img/3/4.jpg", "img/3/5.jpg", "img/3/6.jpg", "img/3/7.jpg", "img/3/8.jpg"]
+}, {
+    "name": "坦坦大炉",
+    "link": "http://www.dianping.com/search/keyword/2/0_坦坦大炉",
+    "img": "img/8/logo.jpg",
+    "introduction": "“坦坦大炉”餐厅五道口店，望京店分别成立于2006年和2012年，是一家以主要经营雪花黑牛类为主的韩式烤肉店，餐厅环境优雅，装修采用了韩国最先进的排风系统，并且...",
+    "more": "“坦坦大炉”餐厅五道口店，望京店分别成立于2006年和2012年，是一家以主要经营雪花黑牛类为主的韩式烤肉店，餐厅环境优雅，装修采用了韩国最先进的排风系统，并且选用了对人体无害的橡树白炭，为您放心吃肉提供了保障，餐厅特聘韩国经营团队，所有酱料均为本餐厅秘制而成，让顾客能够品尝及感受到真正的韩国料理及服务文化。餐厅本着“我们用为父母制作饭菜的诚意来接待您”的经营理念，努力为每位顾客提供满意的菜品和服务而时刻努力着。",
+    "promotion": "http://www.dianping.com/promo/221796?source=6",
+    "coupon": "",
+    "booking": "",
+    "member": "",
+    "carousel": ["img/8/1.jpg", "img/8/2.jpg", "img/8/3.jpg", "img/8/4.jpg", "img/8/5.jpg", "img/8/6.jpg", "img/8/7.jpg", "img/8/8.jpg"]
+}, {
+    "name": "黔惑贵州味",
+    "link": "http://www.dianping.com/search/keyword/2/0_%E9%BB%94%E6%83%91%E8%B4%B5%E5%B7%9E%E5%91%B3",
+    "img": "img/5/logo.jpg",
+    "introduction": "“黔惑”贵州味：原汁原味的贵州菜，全部由贵州厨师主理。以贵州凯里酸汤鱼为招牌菜，讲究将小西红柿经过腌制，加入用米汤发酵而成的米酸里调制，鱼肉鲜嫩，酸爽入味，汤汁...",
+    "more": "“黔惑”贵州味：原汁原味的贵州菜，全部由贵州厨师主理。以贵州凯里酸汤鱼为招牌菜，讲究将小西红柿经过腌制，加入用米汤发酵而成的米酸里调制，鱼肉鲜嫩，酸爽入味，汤汁醇厚。店门口摆放着几口大坛子，就是“发酵工场”。酸菜豆米火锅、酸汤蹄花、青岩米豆腐、蕨粑炒腊肉、苗家豆腐圆子、苗家豆花鱼、花溪牛肉面、肠旺面……这些贵州经典特色菜都保持了贵州食材的本色，本味！",
+    "promotion": "http://www.dianping.com/promo/220065?source=6",
+    "coupon": "http://t.dianping.com/deal/6092240",
+    "booking": "http://www.dianping.com/shop/5433135#reserve",
+    "member": "http://www.dianping.com/card/detail/c3_b5433135_a22",
+    "carousel": ["img/5/1.jpg", "img/5/2.jpg", "img/5/3.jpg", "img/5/4.jpg", "img/5/5.jpg", "img/5/6.jpg", "img/5/7.jpg"]
+},{
+    "name": "火宴山自助火锅",
+    "link": "http://www.dianping.com/shop/5422956",
+    "img": "img/2/logo.jpg",
+    "introduction": "新疆风味,健康饮食火宴山餐饮集团是一家致力于餐饮事业创新与开发,推崇新疆民族文化,倡导快乐健康饮食消费理念的综合性事业机构。目前, 集团旗下拥有小尕子、新疆第一...",
+    "more": "新疆风味,健康饮食火宴山餐饮集团是一家致力于餐饮事业创新与开发,推崇新疆民族文化,倡导快乐健康饮食消费理念的综合性事业机构。目前, 集团旗下拥有小尕子、新疆第一盘、火宴山、阿拉斯加、长福宫等 五个定位不同的餐饮品牌。新鲜美食,缤纷滋味 -   火宴山在北京的第一家店开在望京,第二家店朝阳北路店于2月 F4 。这里新疆风味美食正宗地道,各地海量特色美味也丝毫不输,先进设施配备的中央厨房和物流配送中心,每一次入口都是新鲜滋味。",
+    "promotion": "http://www.dianping.com/promo/214558?source=1",
+    "coupon": "http://t.dianping.com/deal/2097463",
+    "booking": "",
+    "member": "",
+    "carousel": ["img/2/1.jpg", "img/2/2.jpg", "img/2/3.jpg", "img/2/4.jpg", "img/2/5.jpg", "img/2/6.jpg", "img/2/7.jpg", "img/2/8.jpg"]
+}, {
+    "name": "小尕子新疆菜",
+    "link": "http://www.dianping.com/shop/13837678",
+    "img": "img/7/logo.jpg",
+    "introduction": "新疆风味，健康饮食?火宴山餐饮集团是一家致力于餐饮事业创新与开发，推崇新疆民族文化，倡导快乐健康饮食消费理念的综合性事业机构。目前， 集团旗下拥有小尕子、新疆第...",
+    "more": "新疆风味，健康饮食?火宴山餐饮集团是一家致力于餐饮事业创新与开发，推崇新疆民族文化，倡导快乐健康饮食消费理念的综合性事业机构。目前， 集团旗下拥有小尕子、新疆第一盘、火宴山、阿拉斯加、长福宫等 五个定位不同的餐饮品牌。  - 新鲜美食，缤纷滋味 -  ?　　火宴山在北京的第一家店开在望京，第二家店朝阳北路店于2月 2日刚刚火热出炉。店内设计弥漫着伊斯兰的气息，*极具*异域风情 。这里新疆风味美食正宗地道，各地海量特色美味也丝毫不输，先进设施配备的中央厨房和物流配送中心，每一次入口都是新鲜滋味。",
+    "promotion": "",
+    "coupon": "http://t.dianping.com/deal/2154279",
+    "booking": "",
+    "member": "",
+    "carousel": ["img/7/1.jpg", "img/7/2.jpg", "img/7/3.jpg", "img/7/4.jpg", "img/7/5.jpg", "img/7/6.jpg", "img/7/7.jpg", "img/7/8.jpg"]
+},  {
+    "name": "湘缘",
+    "link": "http://www.dianping.com/shop/13919343",
+    "img": "img/4/logo.jpg",
+    "introduction": "北京湘缘餐厅是有一群热爱学习、事业、生活、美食的一群吃货同学共同投资成立。本公司主营湘菜、家常菜、环境优雅是北京唯一一家学习餐厅在这里不经可以享受美食还可以交流...",
+    "more": "北京湘缘餐厅是有一群热爱学习、事业、生活、美食的一群吃货同学共同投资成立。本公司主营湘菜、家常菜、环境优雅是北京唯一一家学习餐厅在这里不经可以享受美食还可以交流学习共同成长。再次我们郑重承诺：一、决不为美食放弃健康；二、决不为利润放弃做人原则。相聚是缘，再聚湘缘。北京湘缘餐厅欢迎大家",
+    "promotion": "",
+    "coupon": "",
+    "booking": "",
+    "member": "",
+    "carousel": ["img/4/1.jpg", "img/4/2.jpg", "img/4/3.jpg", "img/4/4.jpg", "img/4/5.jpg", "img/4/6.jpg", "img/4/7.jpg"]
+}, {
+    "name": " 碰碰凉-不加一滴水的冰淇淋",
+    "link": "http://www.dianping.com/search/keyword/2/0_%E7%A2%B0%E7%A2%B0%E5%87%89-%E4%B8%8D%E5%8A%A0%E4%B8%80%E6%BB%B4%E6%B0%B4%E7%9A%84%E5%86%B0%E6%B7%87%E6%B7%8B",
+    "img": "img/10/logo.jpg",
+    "introduction": "碰碰凉不加一滴水的冰淇淋，源于进口奶源，开创不加水的冰淇淋新纪元",
+    "more": "",
+    "promotion": "",
+    "coupon": "",
+    "booking": "",
+    "member": "",
+    "carousel": ["img/10/1.jpg", "img/10/2.jpg", "img/10/3.jpg", "img/10/4.jpg", "img/10/5.jpg", "img/10/6.jpg", "img/10/7.jpg", "img/10/8.jpg"]
+}
+, {
+    "name": "臻海阁",
+    "link": "http://www.dianping.com/shop/17942312",
+    "img": "img/6/logo.jpg",
+    "introduction": "「五星级的品质，小酒楼的价格！」臻海阁，一处融合东西方美学的餐厅，特聘外国设计师精心打造整体空间，突显兼具港式茶楼与高级粤菜海鲜餐厅的餐饮特色，带给您惊喜的同时...",
+    "more": "「五星级的品质，小酒楼的价格！」臻海阁，一处融合东西方美学的餐厅，特聘外国设计师精心打造整体空间，突显兼具港式茶楼与高级粤菜海鲜餐厅的餐饮特色，带给您惊喜的同时以高品质的服务营造出最为舒适的就餐环境。臻海阁是融合型餐厅，讲求中菜西吃，以不拘一格的烹饪手法打造全新的菜品，以「海鲜」丶「粤菜」丶「下午茶」为三大元素。菜色地道丶多样化，道道用料扎实丶强调传统精致口感，带给您全新却值得回味良久的舌尖享受。特聘厨师香福根，生于香港，精通中国各大菜系，擅长以纯正港式手艺演绎地道佳肴。曾在香港丶北京丶上海的各大知名集团及五星级酒店担任行政总厨，如今将以其丰富的经验为您献上绝美的视觉与味觉双重体验。",
+    "promotion": "",
+    "coupon": "",
+    "booking": "",
+    "member": "",
+    "carousel": ["img/6/1.jpg", "img/6/2.jpg", "img/6/3.jpg", "img/6/4.jpg", "img/6/5.jpg", "img/6/6.jpg", "img/6/7.jpg", "img/6/8.jpg", "img/6/9.jpg", "img/6/10.jpg"]
+},{
+    "name": "烤鱼吧",
+    "link": "http://www.dianping.com/shop/17734210",
+    "img": "img/9/logo.jpg",
+    "introduction": " 味美浓香，香、辣、鲜融为一体 烤鱼吧长期致力于烤鱼的烹饪加工，以独家秘制配方对原材料进行腌制，然后放在炭火上烤至九成熟、辅以新鲜的时蔬和豆制品，将重庆...",
+    "more": "味美浓香，香、辣、鲜融为一体   　　烤鱼吧长期致力于烤鱼的烹饪加工，以独家秘制配方对原材料进行腌制，然后放在炭火上烤至九成熟、辅以新鲜的时蔬和豆制品，将重庆烤鱼的色、香、味及香、辣、鲜融为一体，外皮香脆、肉质软嫩、口感独特、回味无穷。大众消费的烤鱼吧，经过老百姓多年的检验，已分布京城各处，十三家直营店都能让您高兴而来，满意而归！",
+    "promotion": "",
+    "coupon": "http://t.dianping.com/deal/6103973",
+    "booking": "http://www.dianping.com/shop/5856222#reserve",
+    "member": "",
+    "carousel": ["img/9/1.jpg", "img/9/2.jpg", "img/9/3.jpg", "img/9/4.jpg"]
+}];
---
+++ case/chengdu/1406-1/data.js
@@ -0,0 +1,71 @@
+pageTracker._trackPageview("events/market/ttzj/chengdu");
+
+window.GROUP_ID = 200;
+
+window.storeList = [{
+    "name": "三宝烤串",
+    "link": "http://www.dianping.com/shop/15091169",
+    "img": "img/1/logo.jpg",
+    "introduction": "三宝烤串始终坚持以中式烧烤为主调，保证食物的新鲜和质量。店内全部采用无烟设备和无烟炭烧烤，从而保持客人用餐时有一个清洁干净的环境。可以安心舒适地在这里享受美味的烧烤。",
+    "more": "",
+    "promotion": "",
+    "coupon": "http://t.dianping.com/deal/2153103",
+    "booking": "",
+    "member": "",
+    "carousel": []
+}, {
+    "name": "郑辣记田螺",
+    "link": "http://www.dianping.com/shop/8301674",
+    "img": "img/2/logo.jpg",
+    "introduction": "郑辣记田螺，十几年老店，深得消费者认可，来郑辣记敞吃田螺，让你辣翻天;不告诉你，我的错;不来，你的错!",
+    "more": "",
+    "promotion": "http://www.dianping.com/promo/221773?source=1",
+    "coupon": "",
+    "booking": "",
+    "member": "",
+    "carousel": []
+}, {
+    "name": "X先生密室",
+    "link": "http:/www.dianping.com/shop/8719535",
+    "img": "img/3/logo.jpg",
+    "introduction": "机械与影像交织的艺术，想象力与现实的巧妙结合，雅致的环境，贴心的服务，打造成都密室，闹市中独矗的城堡，超现实的真实感，如身临电影中的体验，欢乐轻松的等候区时不时...",
+    "more": "机械与影像交织的艺术，想象力与现实的巧妙结合，雅致的环境，贴心的服务，打造成都密室，闹市中独矗的城堡，超现实的真实感，如身临电影中的体验，欢乐轻松的等候区时不时会听到从楼上传来的阵阵惊叹声，平添了一份紧张的密室感。古董布置的房间，精心的手绘，独特的道具与机关如艺术品般呈现在客人面前，游戏的过程仿佛在享受视觉与思维的盛宴。智慧的您不妨来体验一下密室带给您的无穷乐趣吧。",
+    "promotion": "",
+    "coupon": "http://t.dianping.com/deal/207478",
+    "booking": "",
+    "member": "",
+    "carousel": []
+}, {
+    "name": "Mani&Pedi大班美甲(九龙仓店)",
+    "link": "http://www.dianping.com/shop/17172955",
+    "img": "img/4/logo.jpg",
+    "introduction": "继GALLERIA的年轻品牌Miss Coco后，我们在成都ifs开设了公司旗下高端美甲美睫门店MANI&PEDI。",
+    "more": "",
+    "promotion": "",
+    "coupon": "http://t.dianping.com/deal/2184914",
+    "booking": "",
+    "member": "",
+    "carousel": []
+}, {
+    "name": "梵绣专业健康护肤中心",
+    "link": "http://www.dianping.com/shop/16964656",
+    "img": "img/5/logo.jpg",
+    "introduction": " 梵绣专业健康护肤中心秉承“用专业的产品和技术，把健康之美传递给更多追求高品质生活的女性”的服务宗旨.",
+    "more": "",
+    "promotion": "",
+    "coupon": "http://t.dianping.com/deal/6027712",
+    "booking": "",
+    "member": "",
+    "carousel": []
+}, {
+    "name": "叶氏变脸秀美肤连锁机构",
+    "link": "http://www.dianping.com/shop/6091092",
+    "img": "img/6/logo.jpg",
+    "introduction": "21年品牌，是国内一家专攻皮肤黑黄斑皱等肌肤问题的特色连锁机构，严格遵守“美肤呼唤绿色，整形回归自然”的企业宗旨，全力打造“非整形同步美肤模式”。治疗前留影，治...",
+    "more": "21年品牌，是国内一家专攻皮肤黑黄斑皱等肌肤问题的特色连锁机构，严格遵守“美肤呼唤绿色，整形回归自然”的企业宗旨，全力打造“非整形同步美肤模式”。治疗前留影，治疗后拍照。是变脸秀长期以来一贯坚持的严谨作风，更是变脸秀赢的蓉城爱美女性青睐的有力保障。",
+    "promotion": "http://www.dianping.com/promo/222128?source=1",
+    "coupon": "http://t.dianping.com/deal/2280151",
+    "booking": "",
+    "member": "",
+    "carousel": []
+}];
